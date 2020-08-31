a150ef22479ada83ddca0820ab6227f968b57d0c
cplpearce <clint.pearce@rocketmail.com>
2020-08-31 15:25:01 -0400

    implemented partial header

--- express_server.js
+++ express_server.js
@@ -7,18 +7,21 @@ const PORT = 8080; // default port 8080
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
-  lhL: 'http://www.lighthouselabs.ca',
-  gOog: 'http://www.google.com',
+  lhl: 'https://www.lighthouselabs.ca',
+  goo: 'https://www.google.com',
+  imo: 'https://ombi.ironmantle.ca',
+  imj: 'https://jellyfin.ironmantle.ca',
+
 };
 
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render('pages/urls_show', templateVars);
+  res.render('urls_show', templateVars);
 });
 
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
-  res.render('pages/urls_index', templateVars);
+  res.render('urls_index', templateVars);
 });
 
 app.get('/hello', (req, res) => {
