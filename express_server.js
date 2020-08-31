2ea762e516099e15520d922c2f07279c5bba6c7c
cplpearce <clint.pearce@rocketmail.com>
2020-08-31 15:41:44 -0400

    added a new url page, body parser

--- express_server.js
+++ express_server.js
@@ -14,18 +14,18 @@ const urlDatabase = {
 
 };
 
-app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render('urls_show', templateVars);
-});
-
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render('urls_show', templateVars);
 });
 
 app.listen(PORT, () => {
